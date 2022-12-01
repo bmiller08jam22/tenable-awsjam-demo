44fa90ab8af6d60e4265033a4487c2f461bb75aa
Tenable.cs <admin@accurics.com>
2022-11-30 20:39:53 -0800

    Auto Remediation By Tenable.cs

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<cidr>"]
     //    cidr_blocks = ["172.31.0.0/28"]
   }
   egress {
